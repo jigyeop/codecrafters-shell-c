213d54da35f1de01158417f1383c2671a5a2a5c3
jigyeop <jyoungC1101@handong.ac.kr>
2026-07-22 03:48:13 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   
-  char command[100];
+  char command[128];
   const char *builtins[] = {"echo", "exit", "type"};
   char *path = getenv("PATH");
   int size = sizeof(builtins) / sizeof(builtins[0]);
@@ -24,24 +24,32 @@ int main(int argc, char *argv[]) {
 		int flag = 0;
 		char *dir = strtok(path, ":");
 		strcat(dir, "/");
-		while(dir != NULL) {
-			if (access(strcat(dir, command), F_OK) == 0) {
-				if (access(strcat(dir, command), X_OK) == 0) {
-					printf("%s is %s", command, dir);
-					flag = 1;
-					break;
-				}
-			}
-				dir = strtok(NULL, ":");
-			}
 
 			for (int i = 0; i < size; i++) {
 				if (!(strcmp(command, builtins[i]))) {
 					printf("%s is a shell builtin\n", builtins[i]);
 					flag = 1;
+					break;
 			}
+			
+			if (!flag && path != NULL) {
+				char *path_copy = strdup(path);
+				char *dir = strtok(path_copy, ":");
+			
+				while(dir != NULL) {
+					char full_path[1024];
+					snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+					
+					if (access(full_path, F_OK | X_OK) == 0) {
+					printf("%s is %s", command, full_path);
+					flag = 1;
+					break;
+				}
+			}
+				dir = strtok(NULL, ":");
+		}
+		free(path_copy);
 
-		
 			if (flag != 1) {
 				printf("%s: not found\n", command);
 			}
