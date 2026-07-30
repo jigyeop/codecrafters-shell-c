998e9e56e6efdb045d2a7b0319f14b0a1437b836
jigyeop <jyoungC1101@handong.ac.kr>
2026-07-31 02:26:41 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,8 +4,15 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+	
+  char command[1024];
 
+ 
   printf("$ ");
+
+  fgets(command, sizeof(command), stdin);
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s : command not found", command);
 
   return 0;
 }
