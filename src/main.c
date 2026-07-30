5305502b3d9ba0143148fb1cfb2afcdb22b2d235
jigyeop <jyoungC1101@handong.ac.kr>
2026-07-31 02:43:46 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-void prompt(); 
+void prompt();
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-	
+
+  char command[1024];
 
   prompt();
 
