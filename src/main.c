feb9ada91f621df8ae1f0f659d55fd0f05e6d657
jigyeop <jyoungC1101@handong.ac.kr>
2026-07-31 02:38:20 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void prompt(); 
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 	
-  char command[1024];
 
- 
-  printf("$ ");
+  prompt();
 
   fgets(command, sizeof(command), stdin);
   command[strcspn(command, "\n")] = '\0';
   printf("%s : command not found", command);
 
   return 0;
+}
+
+void prompt() {
+	printf("$ ");
 }
