46bf99219823e4ad0646a8a2856c761dfe64c1d1
jigyeop <jyoungC1101@handong.ac.kr>
2026-07-31 03:44:47 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
- int prompt();
+int shell();
 
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   while(1) {
-	  if (!prompt()) {
+	  if (!shell()) {
 		  break;
 	  }
   }
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-int prompt() {
+int shell() {
 	char *command = NULL;
 	size_t size = 0;
 	
@@ -29,6 +29,11 @@ int prompt() {
 		free(command);
 		return 0;
 	}
+
+	if (!strcmp("echo", command)) {
+		printf("%s\n", command + size);
+		free(command);
+		return 1;
 
 	printf("%s: command not found\n", command);
 	
