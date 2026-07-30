ffce35e4993669ac919e6d4fd5890bd3036a1e9a
jigyeop <jyoungC1101@handong.ac.kr>
2026-07-31 03:26:57 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,16 +8,22 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   prompt();
-  printf("%s : command not found", command);
-
+  
   return 0;
 }
 
 void prompt() {
 	char *command = NULL;
 	size_t size = 0;
+	
 	printf("$ ");
+	
 	getline(&command, &size, stdin);
+
+	command[strcpn(command, '\n')] = '\0';
+
+	printf("%s: command not found", command);
+
 	free(command);
 
 }
