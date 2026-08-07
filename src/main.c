6b8ff5166d0ec7121ac1be508ad1de20203c1c65
jigyeop <jyoungC1101@handong.ac.kr>
2026-08-07 15:20:40 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -43,8 +43,6 @@ int shell() {
 
 			if (strncmp(ptr, "type", spc) == 0 && "type"[spc] == '\0') {
 				ptr += spc;
-				ptr += strcspn(ptr, ws);
-
 				for (int i = 0; i < num_builtins; i++) {
 					if (strcmp(ptr, builtins[i]) == 0) {
 						printf("%.*s is a builtin", ptr);
